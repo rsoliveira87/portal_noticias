4ac0f60469a06cf15e941dd1045cc8924be9b9ac
Robson da Silva Oliveira <cin_rooliveira@uolinc.com>
2017-11-14 12:19:00 -0200

    Criação de templates de header e footer para reaproveitar nas outras views

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,4 +2,8 @@ module.exports = function(application) {
 	application.get('/', function(req, res){
 		application.app.controllers.home.index(application, req, res);
 	});
+
+	application.get('/index', function(req, res){
+		application.app.controllers.home.index(application, req, res);
+	});
 };
